c7593565682f06d0e55e44da3043d23cae0e64d0
Mika <61342907+Mikhail666999@users.noreply.github.com>
2022-06-13 20:14:32 +0500

    Add files via upload

--- assets/zlayer/Zlayer.gd
+++ assets/zlayer/Zlayer.gd
@@ -12,53 +12,20 @@ var hor_l = 0;
 var ver_r = 0;
 var hor_r = 0;
 
-var sens = 2
+var sens = 6.5
 var speed = 15;
-var jump = 10;
-var gravity = 6;
+var jump = 50;
+var gravity = 8;
 
 onready var head = $head
 
 
 ##########################################
 func _ready():
+	gravity /= 3;
 #	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	pass
-	
-func _input(event):
-#	if event is InputEventMouseMotion:
-#		rotate_y(deg2rad(-event.relative.x * Mouse_sensitivity)) 
-#		head.rotate_x(deg2rad(-event.relative.y * Mouse_sensitivity)) 
-#		head.rotation.x = clamp(head.rotation.x, deg2rad(-90), deg2rad(90))
-	
-	rotate_y(deg2rad(hor_r * sens * -1)) 
-	head.rotate_x(deg2rad(ver_r * sens)) 
-	head.rotation.x = clamp(head.rotation.x, deg2rad(-90), deg2rad(90))
-	
 
-func _physics_process(delta):
-	direction = Vector3()
-	move_and_slide(fall, Vector3.UP)
-
-	if not is_on_floor():
-		fall.y -= gravity
-
-	if ver_l < 0:
-		direction -= transform.basis.z * ver_l
-		print(direction)
-	if ver_l > 0:
-		direction += transform.basis.z * ver_l * -1
-		print(direction)
-		
-	if hor_l < 0:
-		direction -= transform.basis.x * hor_l * -1
-	elif hor_l > 0:
-		direction += transform.basis.x * hor_l
-		
-	direction = direction.normalized()
-	velocity = velocity.linear_interpolate(direction * speed, 10 * delta) 
-	velocity = move_and_slide(velocity, Vector3.UP) 
-	
 
 func _process(delta):
 	hor_l = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
@@ -67,13 +34,49 @@ func _process(delta):
 	hor_r = Input.get_action_strength("cam_right") - Input.get_action_strength("cam_left")
 	ver_r = Input.get_action_strength("cam_up") - Input.get_action_strength("cam_down")
 	
+	keyboard();
 	
+	print(ver_l)
+	print(hor_l)
+
+
+func _physics_process(delta):
+	
+	##################### camera
+#	rotate_y(deg2rad(hor_r * sens * -1)) 
+#	head.rotate_x(deg2rad(ver_r * sens)) 
+#	head.rotation.x = clamp(head.rotation.x, deg2rad(-90), deg2rad(90))
+	
+	rotate_y(deg2rad(hor_r * sens * -1)) 
+	head.rotate_x(deg2rad(ver_r * sens)) 
+	head.rotation.x = clamp(head.rotation.x, deg2rad(-90), deg2rad(90))
+
+	
+	direction = Vector3()
+	move_and_slide(fall, Vector3.UP)
+
+	if not is_on_floor():
+		fall.y -= gravity
+
+	if ver_l < 0:
+		direction -= transform.basis.z * ver_l
+	elif ver_l > 0:
+		direction += transform.basis.z * ver_l * -1
+		
+	if hor_l < 0:
+		direction -= transform.basis.x * hor_l * -1
+	elif hor_l > 0:
+		direction += transform.basis.x * hor_l
+	
+#	direction = direction.normalized()
+	velocity = velocity.linear_interpolate(direction * speed, 10 * delta) 
+	velocity = move_and_slide(velocity, Vector3.UP) 
+
+
+func keyboard():
 	if Input.is_action_just_pressed("back"):
 		get_tree().quit()
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		fall.y += jump * Input.get_action_strength("jump");
-		
 	
 
 ########################################
